6731e4998008c5fa49017a7638d61d98b93c95e5
taco <tylerchall16@gmail.com>
2022-08-08 19:01:34 -0500

    test commit

--- src/components/PokeList.tsx
+++ src/components/PokeList.tsx
@@ -7,24 +7,24 @@ const PokeList = ({ pokemon }: { pokemon: { results: Array<any> } }) => {
 	console.log(pokemon);
 
 	return (
-		<>
-			{pokemon.results.map((pokeman: any, index: number) => {
-				<div className="w-6/12 h-6/12" key={index}>
-					<Link href={`/pokemon?id=${index + 1}`}>
-						<a className="border p-4 border-grey my-2 hover:shadow-md capitalize flex items-center text-lg bg-gray-200 rounded-md">
-							<Image
-								src={pokeman.image}
-								alt={pokeman.name}
-								layout="fill"
-								className=" mr-3"
-							/>
-							<span className="mr-2 font-bold">{index + 1}.</span>
-							{pokeman.name}
-						</a>
-					</Link>
-				</div>;
-			})}
-		</>
+		// <>
+		// 	{pokemon.results.map((pokeman: any, index: number) => {
+		// 		<div className="w-6/12 h-6/12" key={index}>
+		// 			<Link href={`/pokemon?id=${index + 1}`}>
+		// 				<a className="border p-4 border-grey my-2 hover:shadow-md capitalize flex items-center text-lg bg-gray-200 rounded-md">
+		// 					<Image
+		// 						src={pokeman.image}
+		// 						alt={pokeman.name}
+		// 						layout="fill"
+		// 						className=" mr-3"
+		// 					/>
+		// 					<span cslassName="mr-2 font-bold">{index + 1}.</span>
+		// 					{pokeman.name}
+		// 				</a>
+		// 			</Link>
+		// 		</div>;
+		// 	})}
+		// </>
 	);
 };
 
